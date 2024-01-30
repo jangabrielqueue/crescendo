5d8b32d09c425db232bf801ea106a3123077b5ae
Jan Gabriel Que(Gab) <jan.que@whiterain-dti.com>
2024-01-30 18:39:51 +0800

    Initiate vite/react, install dependencies + tailwind theme

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  console.log(import.meta.env)
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
+import react from '@vitejs/plugin-react'
+import compression from 'vite-plugin-compression2'
+import { fileURLToPath, URL } from 'url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	build: {
+		sourcemap: false,
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vendor: ['react', 'react-dom']
+				}
+			}
+		}
+	},
+	plugins: [react(), visualizer({ template: 'treemap', brotliSize: true, gzipSize: true }), splitVendorChunkPlugin(),
+		compression({
+			algorithm: 'gzip', exclude: [/\.(br)$ /, /\.(gz)$/]
+		}),
+		compression({
+			algorithm: 'brotliCompress', exclude: [/\.(br)$ /, /\.(gz)$/]
+		})
+	],
+	resolve: {
+		alias: [
+			{ find: '@components', replacement: fileURLToPath(new URL('./src/components', import.meta.url)) },
+		]
+	},
+	server: {
+		// this ensures that the browser opens upon server start
+		open: true,
+		// this sets a default port to 3000
+		port: 3000
+	},
+})
