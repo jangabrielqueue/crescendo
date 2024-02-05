34bb3130b1822783675540120582a0c222206015
Jan Gabriel Que(Gab) <jan.que@whiterain-dti.com>
2024-02-05 17:11:58 +0800

    Complex Pagination component

---
+++ src/components/ComplexPagination/index.tsx
@@ -0,0 +1,58 @@
+import { Button, Flex, Grid } from '@tremor/react'
+import { ComplexPaged } from '@utils/interface'
+
+type ComplexPaginationProps = {
+	onPageChange: (newCursor: number) => void
+} & ComplexPaged
+
+const styles = {
+	button: 'rounded-full border-tremor-border text-tremor-content dark:border-dark-tremor-border dark:text-dark-tremor-content'
+}
+
+const ComplexPagination = ({ cursor, noMoreData, onPageChange }: ComplexPaginationProps): JSX.Element | null => {
+	if (cursor === 1 && noMoreData) return null
+	const handlePageChange = (type: 1 | 'prev' | 'next') => {
+		let newCursor = cursor
+		if (type === 1) {
+			newCursor = 1
+		} else if (type === 'prev') {
+			newCursor--
+		} else {
+			newCursor++
+		}
+		onPageChange(newCursor)
+	}
+	return (
+
+		<Grid className='gap-2 my-2' numItems={2}>
+			<Button
+				className={styles.button}
+				variant='secondary'
+				disabled={cursor === 1}
+				onClick={() => handlePageChange(1)}
+			>
+				First
+			</Button>
+			<Flex justifyContent='center'>
+				<Button
+					className={`${styles.button} rounded-r-none`}
+					variant='secondary'
+					disabled={cursor === 1}
+					onClick={() => handlePageChange('prev')}
+				>
+					prev
+				</Button>
+				<Button
+					className={`${styles.button} rounded-l-none`}
+					variant='secondary'
+					disabled={noMoreData}
+					onClick={() => handlePageChange('next')}
+				>
+					next
+				</Button>
+			</Flex>
+		</Grid>
+	)
+}
+
+export default ComplexPagination
--- src/utils/interface.ts
+++ src/utils/interface.ts
@@ -1,1 +1,11 @@
 export type ObjectValues<T> = T[keyof T]
+
+export type ComplexPaged = {
+	cursor: number,
+	size: number,
+	noMoreData: boolean
+}
+
+export type ComplexPagedData<T> = {
+	items: T[]
+} & ComplexPaged
